a1258b050162d738e91791d3be1c872227c98dfa
Nahuel-prog23 <nahueljuanveraguardia@gmail.com>
2024-08-14 21:28:29 -0300

    Componente a que hora juega Boquita

---
+++ src/components/Countdown.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+interface CountdownProps {
+  targetDate: string; // La fecha y hora objetivo en formato "YYYY-MM-DDTHH:mm:ss"
+}
+
+const Countdown: React.FC<CountdownProps> = ({ targetDate }) => {
+  const calculateTimeLeft = () => {
+    const difference = new Date(targetDate).getTime() - new Date().getTime();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        días: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        horas: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutos: Math.floor((difference / 1000 / 60) % 60),
+        segundos: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents: JSX.Element[] = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (timeLeft[interval]) {
+      timerComponents.push(
+        <span key={interval}>
+          {timeLeft[interval]} {interval}{" "}
+        </span>
+      );
+    }
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? timerComponents : <span>¡Está jugando Boquita!</span>}
+    </div>
+  );
+};
+
+export default Countdown;
--- src/components/Tridente.js
+++ src/components/Tridente.js
@@ -1,13 +1,19 @@
 import TridenteItemNuevo from './TridenteItemNuevo';
+import Countdown from './Countdown';
 import '../index.css';
 
 function Tridente({counters}) {
-  return (
-    <div className="contenedor">      
+  return (  
+    <>  
+      <h2>A que hora juega Boquita</h2>
+      <Countdown targetDate="2024-08-15T21:30:00" />
+   
+    <div className="contenedor">  
       {counters.map((counters) => (
         counters.img && <TridenteItemNuevo key={counters.id}  player={counters}/>
       ))}    
     </div>
+    </>
   );
 }
 
